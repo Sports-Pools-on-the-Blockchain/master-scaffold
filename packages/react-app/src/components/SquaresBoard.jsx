e764012c100316f3529bce4a7776ceb451a74d2f
jordang7 <77jgreenberg77@gmail.com>
2022-05-18 15:52:46 -0400

    formatting

--- packages/react-app/src/components/SquaresBoard.jsx
+++ packages/react-app/src/components/SquaresBoard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { Grid, GridItem, Text, Center } from "@chakra-ui/react";
 export default function SquaresBoard({ eventID }) {
   useEffect(() => {
     async function getSquaresFromContract() {
@@ -11,12 +11,15 @@ export default function SquaresBoard({ eventID }) {
   return (
     <>
       <Grid templateColumns="repeat(11, 1fr)" templateRows="repeat(11, 1fr)" gap={0} p={10}>
-        <GridItem rowSpan={1} colSpan={11} bg="beige">
-          Home team
+        <GridItem rowSpan={1} colSpan={11} bg="beige" pt={"10px"}>
+          <Center>
+            <Text>Home team</Text>
+          </Center>
         </GridItem>
-        <GridItem rowSpan={11} colSpan={1} bg="beige">
-          {" "}
-          Away Team
+        <GridItem rowSpan={11} colSpan={1} bg="beige" pt={"10px"}>
+          <Center>
+            <Text sx={{ writingMode: "vertical-rl", transform: "rotate(180deg)" }}>Away Team</Text>
+          </Center>
         </GridItem>
         {Array.from(new Array(110), (val, index) => (
           <GridItem w="100%" h="10" border={"2px"}>
